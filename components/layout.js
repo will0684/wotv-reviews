736f5aa14e03558372d84c7e517340171266af28
will0684 <jordan.walcaraz@gmail.com>
2021-03-04 13:19:25 -0500

    removed head from layout component

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,9 @@
-import Head from 'next/head'
 import PropTypes from 'prop-types'
 
 // Application layout component
 export default function Layout({children}) {
   return (
     <div className="mt-12 mb-24 sm:px-4">
-      <Head>
-        <meta
-          name="description"
-          content="view information on wotv units and vision cards"
-        />
-      </Head>
       <main>{children}</main>
     </div>
   )
