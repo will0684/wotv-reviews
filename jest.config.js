06f02230097f162c63876e76badcf0b851a117b4
will0684 <jordan.walcaraz@gmail.com>
2021-03-04 12:41:57 -0500

    Added jest config for code coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-  }
+  },
+  "coverageThreshold": {
+    "global": {
+    "branches": 0,
+    "functions": 0,
+    "lines": 0,
+    "statements": 0
+    }
+  },
 };
